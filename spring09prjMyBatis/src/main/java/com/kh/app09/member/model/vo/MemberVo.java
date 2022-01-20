b0484d3226fcecc2bf246da3ee82249eb5883d68
lecharl <lecharlhi@gmail.com>
2022-01-20 23:59:16 +0900

    220120_약간의변경, spring09

---
+++ spring09prjMyBatis/src/main/java/com/kh/app09/member/model/vo/MemberVo.java
@@ -0,0 +1,26 @@
+package com.kh.app09.member.model.vo;
+
+public class MemberVo {
+	private String id;
+	private String pwd;
+	private String nick;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getNick() {
+		return nick;
+	}
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+}
